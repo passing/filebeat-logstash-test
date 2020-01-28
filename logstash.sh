b16dd3b328961c902d8a9af0a36a6977f0dd2097
Martin Passing <martin.passing@zooplus.com>
2020-01-28 17:03:39 +0100

    fix filebeat config

--- logstash.sh
+++ logstash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 docker build -t logstash-beats logstash
-docker run -ti --rm --env-file logstash.env -m 2048M --name logstash logstash-beats
+docker run -ti --rm -p 9600:9600 --env-file logstash.env -m 2048M --name logstash logstash-beats
